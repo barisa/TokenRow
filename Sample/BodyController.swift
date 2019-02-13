19cbc1609c1e438adfe840371e87477e7465af1c
Alex Nolasco <alex.nolasco@chronwell.com>
2019-02-13 09:14:31 -0500

    Updated to CLTokenInputView 2.4.5

--- Sample/BodyController.swift
+++ Sample/BodyController.swift
@@ -8,17 +8,19 @@
 import Foundation
 
 final class BodyController {
-    
+
     
     func data() -> [BodyPart] {
         return [
             BodyPart(id: "1", description:"Ear"),
             BodyPart(id: "2", description:"Arm"),
-            BodyPart(id: "3", description:"Leg"),
+            BodyPart(id: "3", description:"Left Leg"),
             BodyPart(id: "4", description:"Head"),
             BodyPart(id: "5", description:"Brain"),
-            BodyPart(id: "6", description:"Leg"),
-            BodyPart(id: "7", description:"The Gallbladder, This is a muscular sac-like structure that is attached to the liver. The gallbladder is around 8 cm in length and 4 cm in diameter")
+            BodyPart(id: "6", description:"Right Leg"),
+            BodyPart(id: "7", description:"The Gallbladder, This is a muscular sac-like structure that is attached to the liver. The gallbladder is around 8 cm in length and 4 cm in diameter"),
+            BodyPart(id: "8", description:"The stomach is a muscular, hollow organ in the gastrointestinal tract of humans and many other animals, including several invertebrates. The stomach has a dilated structure and functions as a vital digestive organ. In the digestive system the stomach is involved in the second phase of digestion, following mastication (chewing).")
+
         ]
     }
     
